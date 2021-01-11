857bf23b39b6867f9a23610bee7e1f5288893587
Michael Akpe <73423974+michae1akpe@users.noreply.github.com>
2021-01-11 08:01:28 +0000

    Update Power Ranking Tracker.py

--- Power Ranking Tracker.py
+++ Power Ranking Tracker.py
@@ -7,14 +7,15 @@ platform = input("\nPlease choose the platform of the user. "
 
 url = 'https://api.fortnitetracker.com/v1/powerrankings/' + platform + '/' + 'EU' + '/' + username
 headers = {
-    'TRN-Api-Key': 'fill with your api key'  # This can be gotten at fortnitetracker.com
+    'TRN-Api-Key': '#Include your TRN-API-Key here'  # This can be gotten at fortnitetracker.com
 }
 
-"""This two lines sends the request to attain platform name, country and epic username"""
-
-response = requests.get(url, headers=headers)
-info = response.json()
 try:
+    """This two lines sends the request to attain platform name, country and epic username"""
+
+    response = requests.get(url, headers=headers)
+    info = response.json()
+
     if response.status_code == 200:
         print('Region:', info['region'])
         print('Username:', info['name'])
@@ -24,8 +25,9 @@ try:
         print('Event Played in:', info['events'])
         print('Rank in Region:', info['rank'])
         print('Win Percentage in Tournaments:', info['percentile'])
-    elif response.status_code == 404:
-        print("Unable to preform that. ")
+    
 
 except:
-    print("Could not find the Power ranking for that user.")
+    if response.status_code == 404:
+        print("\nCould not find the Power ranking for that user. Most likely the user has no Power Ranking.")
+    
